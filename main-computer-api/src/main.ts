b071e9118aec2c7427dbff4aa13a0b2e50c74dbd
Ivã Munhoz <iamunhoz@gmail.com>
2023-07-26 10:25:34 -0300

    swagger

--- main-computer-api/src/main.ts
+++ main-computer-api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Starship Main Computer')
+    .setDescription('The main computer API routes')
+    .setVersion('1.0')
+    .addTag('starship')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
